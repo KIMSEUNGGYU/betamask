1553aa113ce76114f33a15f314a38f1852752690
kimseunggyu <dev.seunggyu@gmail.com>
2019-12-15 23:07:03 +0900

    [feat] 제곱근 계산 기능 구현
    - 유한체 제곱근 기능 구현

--- src/s256Field.py
+++ src/s256Field.py
@@ -16,3 +16,7 @@ class S256Field(FieldElement):
         # 원소를 화면에 보여줄때 256bit 자리를 채우기 위함. 빈자리가 생기면 b'0'으로 채움.
         # 256 bit -> 32byte -> 16진수로 64자리
         return '{:x}'.format(self.num).zfill(64) # 16진수로 넘어온 수를 표현, 단 64자리
+
+    def sqrt(self):
+        ## [(secp256k1) 에서] 직렬화할때 제곱근을 계산할때 w = v^(p+1)/4 임!
+        return pow(self, (PRIME+1) // 4)
