2e4ba912795281d633290216b595b64b7be55421
kimseunggyu <dev.seunggyu@gmail.com>
2019-12-15 23:11:02 +0900

    [feat] 비밀키 직렬화 기능 구현
    - 개인키(비밀키) 를 직렬화하는 기능 구현 (wif)

--- src/privatekey.py
+++ src/privatekey.py
@@ -1,6 +1,7 @@
 from lib.config import (GX, GY, N)
 from src.s256Point import S256Point
 from src.signature import Signature
+from lib.helper import (encode_base58_checksum)
 
 ## 안전한 키값을 생성하기 위해 필요
 import hashlib
@@ -69,3 +70,31 @@ class PrivateKey:
                 return candidate  # k 값으로 알맞은 값
             k = hmac.new(k, v + b'\x00', s256).digest()
             v = hmac.new(k, v, s256).digest()
+
+    def wif(self, compressed=True, testnet=False):
+        """
+        비밀키 직렬화 - 비밀키를 읽기 쉽도록 (잘 사용하지 않고, 주로 지갑을 다른 지갑을 옮기고 싶을때 사용)
+
+        비밀키 직렬화(wif) 생성 방식
+        1. 메인넷 접두어 0x80, 테스트넷 접두어 0xef 로 시작
+        2. 비밀키를 32바이트 길이의 빅엔디언으로 표현
+        3. 대응하는 공개키를 압축 sec 로 표현했다면 2번 결과 뒤에 0x01 추가
+        4. 1단계 결과 + 2단계 결과 + 3단계 결과
+        5. 4번 결과에 hash256 하고 checksum 구하기 [:4]
+        6. 4번 결과 + 5번 결과 후 Base58 로 부호화
+
+        :param compressed:  sec 압축 방식이었는지
+        :param testnet:     테스트넷 wif 를 생성할지
+        :return:
+        """
+
+        secret_bytes = self.secret.to_bytes(32, 'big')  # 2 단계
+        if testnet:                                     # 1 단계
+            prefix = b'\xef'
+        else:
+            prefix = b'\x80'
+        if compressed:                                  # 3 단계
+            suffix = b'\x01'
+        else:
+            suffix = b''
+        return encode_base58_checksum(prefix + secret_bytes + suffix)   # 4 단계 수행 후 encode_base58_checksum 기능 수행(5, 6 단계)
