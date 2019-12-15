233d0f78ee18c85f1fbc793b9b6769c1a0dfbdf0
kimseunggyu <dev.seunggyu@gmail.com>
2019-12-15 23:04:41 +0900

    [feat] hash160, base58 관련 기능 구현
    - hash160 기능 구현 (hash160)
    - base58 부호화 기능 구현 (encode_base58)
    - base58 체크섬 및 (비크코인 주소 생성) 기능 구현 (ecnode_base58_checksum)
    - base58 디코드 기능 구현 (decode_base58)
    
    - 바이트 타입을 정수로 변환 - 리틀엔디언 (little_endian_to_int)
    - 정수를 바이트로 변환 - 리틀엔디언 (int_to_little_endian)

--- lib/helper.py
+++ lib/helper.py
@@ -1,11 +1,101 @@
 from unittest import TestSuite, TextTestRunner
 import hashlib
 
+BASE58_ALPHABET = '123456789ABCDEFGHJKLMNPQRSTUVWXYZabcdefghijkmnopqrstuvwxyz'
+
+## TEST 실행
 def run(test):
     suite = TestSuite()
     suite.addTest(test)
     TextTestRunner().run(suite)
 
+## HASH
 def hash256(str):
     '''sha256 두 번 반복'''
     return hashlib.sha256(hashlib.sha256(str).digest()).digest()
+
+def hash160(str):
+    '''sha256 -> ripemd160 수행'''
+    return hashlib.new('ripemd160', hashlib.sha256(str).digest()).digest()
+
+
+## BASE 58
+def encode_base58(str):
+    """
+    임의 길이의 bytes 형 값을 받아 Base58로 부호화된 string 형 값을 반환
+    :param str:
+    :return:
+    """
+    ## p2pkh 에서 필요한 작업 수행 - 앞에 몇 byte가 0인지 기억했다가 -> 그 갯수만큼 1로 만듦
+    count = 0
+    for char in str:
+        if char == 0:
+            count += 1
+        else:
+            break
+
+    num = int.from_bytes(str, 'big')
+    prefix = '1' * count
+    result = ''
+
+    ## 58진수로 수 체계 생성 (BASE58 수체계)
+    while num > 0:
+        num, mod = divmod(num, 58)
+        result = BASE58_ALPHABET[mod] + result
+
+    return prefix + result
+
+
+def encode_base58_checksum(b):
+    """
+    base58 로 부호화도 하고, 그 중간에 checksum도 넣고
+
+    비트코인을 구하는 방식은
+    1. 메인넷은 0x00 으로 시작, 테스트넷은 0x6f 로 시작
+    2. sec 형식 주소를 hash160 방식을 사용해서 출력값을 얻음
+    3. 1단계의 값과 2단계의 값을 합침
+    4. 3에서 얻은 결과를 hash256 하고, 그 결과의 첫 4바이트 취함 <- checksum 구하기
+    5. 3의 결과 뒤에 4의 결과를 붙이고 BASE58로 부호화
+
+    가  있는데 그 중에서 4, 5 방식을 처리, 입력받은 파리미터 값이 3 단계의 값
+
+
+    :param b: prefix(네트워크 종류) + sec 값 합친 값 - bytes 타입
+    :return:
+    """
+    return encode_base58(b + hash256(b)[:4])
+
+
+## 내가 안 짬
+def decode_base58(s):
+    num = 0
+    for c in s:
+        num *= 58
+        num += BASE58_ALPHABET.index(c)
+    combined = num.to_bytes(25, byteorder='big')
+    checksum = combined[-4:]
+    if hash256(combined[:-4])[:4] != checksum:
+        raise ValueError('bad address: {} {}'.format(checksum, hash256(combined[:-4])[:4]))
+    return combined[1:-4]
+
+
+
+## 바이트 타입을 -> 정수형으로 반환 (리틀엔디언, 빅엔디언)
+def little_endian_to_int(bytes):
+    """
+    byte 타입 값을 읽어서 리틀엔디언 정수로 반환
+    :param bytes: 바이트 값
+    :return:
+    """
+    return int.from_bytes(bytes, 'little')
+
+
+def int_to_little_endian(n, length):
+    """
+    정수를 받아 리틀엔디언으로 bytes 형 값으로 반환
+    :param n:       정수 n
+    :param length:  bytes 갯수
+    :return:
+        bytes: 정수 n 의 리틀엔디언 방식 바이트값
+    """
+    return n.to_bytes(length, 'little')
