7d81b1732332bf77201ec764942b20a7b1356c01
kimseunggyu <dev.seunggyu@gmail.com>
2019-12-17 11:00:52 +0900

    [feat] transactionFetcher 분석 필요
    - 테스트를 하기 위해서는 필요한 기능
    - 원래는 노드들이 갖고 있는 UTXO 값을 이용해서 입력, 출력들을 찾아야 하는데 책에서는 이를 3자 서비스를 이용하도록함.
    - 3자 서비스를 이용할때 사용함.
    
    - 3자 서비스 네트워크 별 지정 (get_url)
    - UTXO? 데이터 지정 - 캐시 공간에 데이터를 저장
    
    - load_cache, dump_cache 분석 필요

---
+++ src/transactionFetcher.py
@@ -0,0 +1,65 @@
+from io import BytesIO
+import requests
+import json
+
+from lib.helper import (little_endian_to_int)
+
+class TxFetcher:
+    cache = {}
+
+    @classmethod
+    def get_url(cls, testnet=False):
+        if testnet:
+            return 'http://testnet.programmingbitcoin.com'
+        else:
+            return 'http://mainnet.programmingbitcoin.com'
+
+    @classmethod
+    def fetch(cls, tx_id, testnet=False, fresh=False):
+        from src.transaction import Transaction
+
+        print("tx_id", tx_id)
+        if fresh or (tx_id not in cls.cache):
+            print("******* in ********")
+            url = '{}/tx/{}.hex'.format(cls.get_url(testnet), tx_id)
+            response = requests.get(url)
+            try:
+                raw = bytes.fromhex(response.text.strip())
+            except ValueError:
+                raise ValueError('unexpected response: {}'.format(response.text))
+            # make sure the tx we got matches to the hash we requested
+            if raw[4] == 0:
+                raw = raw[:4] + raw[6:]
+                tx = Transaction.parse(BytesIO(raw), testnet=testnet)
+                tx.locktime = little_endian_to_int(raw[-4:])
+            else:
+                tx = Transaction.parse(BytesIO(raw), testnet=testnet)
+            if tx.id() != tx_id:
+                raise ValueError('not the same id: {} vs {}'.format(tx.id(), tx_id))
+            cls.cache[tx_id] = tx
+        cls.cache[tx_id].testnet = testnet
+
+        # print("cache", cls.cache)
+        return cls.cache[tx_id]
+
+    @classmethod
+    def load_cache(cls, filename):
+        from src.transaction import Transaction
+        disk_cache = json.loads(open(filename, 'r').read())
+        for k, raw_hex in disk_cache.items():
+            raw = bytes.fromhex(raw_hex)
+            if raw[4] == 0:
+                raw = raw[:4] + raw[6:]
+                tx = Transaction.parse(BytesIO(raw))
+                tx.locktime = little_endian_to_int(raw[-4:])
+            else:
+                tx = Transaction.parse(BytesIO(raw))
+            cls.cache[k] = tx
+
+    @classmethod
+    def dump_cache(cls, filename):
+        with open(filename, 'w') as f:
+            to_dump = {k: tx.serialize().hex() for k, tx in cls.cache.items()}
+            s = json.dumps(to_dump, sort_keys=True, indent=4)
+            f.write(s)
+
