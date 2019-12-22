b57039dd8525f1d0c1e0b9156f5cc929d3b017b4
kimseunggyu <dev.seunggyu@gmail.com>
2019-12-22 17:14:11 +0900

    [feat] 트랜잭션 서명해시(z) 생성 기능,  트랜잭션 유효성검증, 각각의 input 검증하는 기능 구현, 해제스크립틔 기능 구현
    - 트랜잭션 서명 해시 (z) 생성 기능 구현 (signature_hash)
    - 트랜잭션 각각의 입력들 검증하는 기능 구현 (verify_input)
    - 트랜잭션 전체 유효성 검증 기능 구현 (verify)
    - 해제 스크립트 기능 구현 (signature_input)

--- src/transaction.py
+++ src/transaction.py
@@ -1,6 +1,8 @@
-from lib.helper import (hash256, little_endian_to_int, read_variant, int_to_little_endian, encode_variant)
+from lib.helper import (hash256, little_endian_to_int, read_variant, int_to_little_endian, encode_variant, SIGNATURE_HASH_ALL)
 from src.transactionIn import TransactionIn
 from src.transactionOut import TransactionOut
+from src.script import Script
+
 
 class Transaction:
     def __init__(self, version, tx_inputs, tx_outputs, locktime, testnet=False):
@@ -121,5 +123,116 @@ class Transaction:
             output_sum += tx_out.amount
         # fee is input sum - output sum
         return input_sum - output_sum
+
+    def signature_hash(self, input_index):
+        """
+        input_index에 서명해야하는 해시의 정수 표현을 리턴
+        결국은 트랜잭션 값을 재정의해서 -> 서명 해시값 구하기 (z)
+
+        input_index 와 연관 있는 트랜잭션 input 객체에 해제스크립트 대신에 잠금 스크립트 넣음
+        1. 버전
+        2. 입력 갯수
+        3. 입력들
+        4. 출력 갯수
+        5. 출력들
+        6. 락타임
+        7. 해시 유형
+
+        :param input_index:
+        :return:
+        """
+        ## 1. 버전 구하기
+        version = int_to_little_endian(self.version, 4)                         # 버전 구하기
+
+        ## 2. 입력 연관
+        inputs_number = encode_variant(len(self.tx_inputs))                     # 입력 갯수 구하기
+        inputs = b''
+        for i, tx_input in enumerate(self.tx_inputs):                           # 입력 트랜잭션 직렬화?
+            if i == input_index:                                                # 내가 찾고하는 Index 라면 해제스크립트 자리에 잠금 스크립트 삽입
+                # print('i == index')
+                # script_signature = tx_input.script_public_key
+                inputs += TransactionIn(
+                    previous_transaction=tx_input.previous_transaction,         # 이전 트랜잭션
+                    previous_index=tx_input.previous_index,                     # 이전 트랜잭션 index
+                    script_signature=tx_input.script_public_key(testnet=True),  # 잠금스크립트 값 넣기 (얘가 핵심)
+                    sequence=tx_input.sequence                                  # 입력 시퀀스 값 넣기
+                ).serialize()
+            else:                                                               # 내가 원하는 index 가 아니면, 해제스크립트 비워두기 ?
+                # print('i !!= index')
+                # script_signature = None
+                inputs += TransactionIn(
+                    previous_transaction=tx_input.previous_transaction,
+                    previous_index=tx_input.previous_index,
+                    sequence=tx_input.sequence
+                ).serialize()
+
+        ## 3. 출력 연관
+        outputs_number = encode_variant(len(self.tx_outputs))   # 출력 갯수 구하기
+        outputs = b''
+        for tx_output in self.tx_outputs:
+            outputs += tx_output.serialize()
+
+
+        ## 4. 락타임
+        locktime = int_to_little_endian(self.locktime, 4)
+
+        ## 5. 해시 유형
+        hash_type = int_to_little_endian(SIGNATURE_HASH_ALL, 4)
+
+        result = version + inputs_number + inputs + outputs_number + outputs + locktime + hash_type
+        hash256_value = hash256(result)
+        return int.from_bytes(hash256_value, 'big')
+
+    def verify_input(self, input_index):
+        """
+        트랜잭션 각각의 입력들 검증
+        :param input_index:
+        :return:
+        """
+
+        tx_input = self.tx_inputs[input_index]
+        # print('tx_input', tx_input)
+        script_public_key = tx_input.script_public_key(testnet=self.testnet)
+        # print('&&script_public_key:', script_public_key)
+        z = self.signature_hash(input_index=input_index)
+        # print('z', z)
+
+        # print('&&tx_input.script_signature:', tx_input.script_signature)
+
+        combined_script = tx_input.script_signature + script_public_key         # 스크립트 값 확인해보기
+        return combined_script.evaluate(z)
+
+    def verify(self):
+        """
+        트랜잭션이 유효한지 검증
+        :return:
+        """
+        """Verify this transaction"""
+        if self.fee() < 0:                      # 수수료가 음수인지 확인 - 코인이 발행되지 않게 하기 위해
+            return False
+
+        for i in range(len(self.tx_inputs)):    # 각 입력이 올바른 해제 스크립트를 가지고 있는지 확인
+            if not self.verify_input(i):
+                print("input error")
+                return False
+
+        return True
+
+    def signature_input(self, input_index, private_key, compressed=True):
+        """
+        해제스크립트 기능 
+        :param input_index:
+        :param private_key:
+        :param compressed:
+        :return:
+        """
+        z = self.signature_hash(input_index)
+        der = private_key.sign(z).der()
+        signature = der + SIGNATURE_HASH_ALL.to_bytes(1, 'big')
+        sec = private_key.point.sec(compressed=compressed)
+
+        # print('tx_in', self.tx_inputs)
+        self.tx_inputs[input_index].script_signature = Script([signature, sec])
+        return self.verify_input(input_index)
 
 
