c9223d09aef89506f963d956d1c9aefca558b31f
kimseunggyu <dev.seunggyu@gmail.com>
2019-12-28 12:10:21 +0900

    [feat] 기존의 users db 를 잘못 구성해 db table 변경
    - 테이블 2개가 필요해 __bind_key__ 를 사용
    - 기존에는 원본 비밀번호를 수정했는데 그렇게하면 서명을 못하기 때문에 로그인용 비밀번호를 열을 새로 생성

--- develop/betamask/models.py
+++ develop/betamask/models.py
@@ -4,10 +4,12 @@ db = SQLAlchemy()
 
 class Users(db.Model):
     __tablename__ = "users"
+    __bind_key__ = 'test2'
 
     id = db.Column(db.Integer, primary_key=True)
     address = db.Column(db.String(64))
     password = db.Column(db.String(100))
+    fake_password = db.Column(db.String(100))
     mnemonic = db.Column(db.String(200))
 
     @property  # 실제로 함수지만, 접근할때 함수가 아닌 변수처럼 사용 가능 - property
@@ -16,5 +18,7 @@ class Users(db.Model):
             'id': self.id,
             'address': self.address,
             'password': self.password,
+            'fake_password': self.fake_password,
             'mnemonic': self.mnemonic
         }
+
