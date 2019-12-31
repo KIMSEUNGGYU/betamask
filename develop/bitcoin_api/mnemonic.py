20c30a487f2f14e1ee8a17a79ca1479c32d2645c
kimseunggyu <dev.seunggyu@gmail.com>
2019-12-31 21:56:35 +0900

    [refactor] 니모닉 코드 불필요한 코드 제거, docstring 작성
    - 니모닉 코드 관련된 코드 중에서 불필요한 코드 제거
    - 파이썬 주석 작성

--- develop/bitcoin_api/mnemonic.py
+++ develop/bitcoin_api/mnemonic.py
@@ -1,61 +1,35 @@
 import os
-path = os.path.split(os.path.abspath(os.path.dirname(__file__)))[0] # 상위 디렉토리 추출
-path = os.path.split(path)[0]                                       # 상위 디렉토리 추출
-
 import sys
-sys.path.append(path)                                               # path 지정
+path = os.path.split(os.path.abspath(os.path.dirname(__file__)))[0]     # 상위 디렉토리 추출
+path = os.path.split(path)[0]                                           # 상위 디렉토리 추출
+sys.path.append(path)                                                   # path 지정
 
-
-from lib.helper import (hash256, little_endian_to_int, decode_base58)
-from lib.mnemonic import Mnemonic
 from src.privatekey import PrivateKey
+from lib.mnemonic import Mnemonic
+from lib.helper import (hash256, little_endian_to_int, decode_base58)
 
 def get_bitcoin_address(password):
-    #######
-    password = password.encode()                                            # 1. 비밀번호를 생성
-    secret = hash256(password)                                              # 2. 비밀번호를 알지 못하게 hash256 으로 해시
-    secret = little_endian_to_int(secret)                                   # 3. Private의 secret 은 정수이므로 바이트 값을 정수로 변환
-    private_key = PrivateKey(secret=secret)                                 # 4. Private 객체 생성 secret 과 공개키 생성
+    """
+    비트코인 주소 가져오기 (생성 후 가져 옴)
+    :param password: 사용자의 비밀번호
+    :return:
+    """
+
+    password = password.encode()                            # 1. 비밀번호를 생성
+    secret = hash256(password)                              # 2. 비밀번호를 알지 못하게 hash256 으로 해시
+    secret = little_endian_to_int(secret)                   # 3. Private의 secret 은 정수이므로 바이트 값을 정수로 변환
+    private_key = PrivateKey(secret=secret)                 # 4. Private 객체 생성 secret 과 공개키 생성
 
     bitcoin_address = private_key.point.address(compressed=True, testnet=True)  # 비밀번호를 이용하여 비트코인 주소 생성
     return bitcoin_address
 
 
 def make_mnemonic(address):
-    # bitcoin_address = get_bitcoin_address(address)
+    """
+    비트코인 주소를 이용해 니모닉 코드 생성
+    :param address:
+    :return:
+    """
     hash160_value = decode_base58(address)
     mnemonic = Mnemonic("english")
     return mnemonic.to_mnemonic(hash160_value)
-
-
-    # print(type(password_phrase))
-    # print(password_phrase)
-    # print('MAKE MNEMONIC')
-    # password = password.encode()        # string to bytes
-    # print(type(password.encode()))
-    # print(password.encode())
-    # return password
-
-
-# password_phrase = b"https://github.com/kimseunggyu gyu's password"      # 1. 비밀번호를 생성
-# 2. 비밀번호를 알지 못하게 hash256 으로 해시
-# secret = hash256(password_phrase)
-# 3. Private의 secret 은 정수이므로 바이트 값을 정수로 변환
-# secret = little_endian_to_int(secret)
-# 4. Private 객체 생성 secret 과 공개키 생성
-# private_key = PrivateKey(secret=secret)
-
-
-# 방법 1
-# publick_key_hash160_value = private_key.point.hash160(compressed=True)  # 비압축 sec 방식 공개키 hash160 수행
-# print("공개키 hash160: ", publick_key_hash160_value.hex())
-# mnemonic = Mnemonic("english")                                          # 니모닉 객체 생성
-# print("mnemonic: ", mnemonic.to_mnemonic(publick_key_hash160_value))    # 니모닉 코드 생성
-
-# # 방법 2
-# # bitcoin_address = private_key.point.address(compressed=True, testnet=True)  # 비밀번호를 이용하여 비트코인 주소 생성
-# # print("bitcoin_address", bitcoin_address)
-# # hash160_value = decode_base58(bitcoin_address)
-# # print("hash160_value", hash160_value.hex())
-# # mnemonic = Mnemonic("english")
-# # print("mnemonic: ", mnemonic.to_mnemonic(hash160_value))
